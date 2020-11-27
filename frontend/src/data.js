112cdcc31627556d26d06845e0375767f0905f7e
Sujan Shahi <sujanshahi@Sujans-MBP.lan1>
2020-11-26 19:47:54 -0600

    First Commit of web app kapStore

---
+++ frontend/src/data.js
@@ -0,0 +1,66 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Girl with stars',
+            category:'Art',
+            image:'/images/product-1.jpg',
+            price: 97.99,
+            rating: 4.5,
+            numReviews: 15,
+            description:'Made with dye'
+        },
+        {
+            _id:'2',
+            name:'Girl with glass',
+            category:'Art',
+            image:'/images/product-2.png',
+            price: 83.99,
+            rating: 4.5,
+            numReviews: 15,
+            description:'Made with love'
+        },
+        {
+            _id:'3',
+            name:'Girl with stars',
+            category:'Shirts',
+            image:'/images/product-1.jpg',
+            price: 120,
+            rating: 4.5,
+            numReviews: 15,
+            description:'Made with dye'
+        },
+        {
+            _id:'3',
+            name:'Girl with stars',
+            category:'Shirts',
+            image:'/images/product-1.jpg',
+            price: 120,
+            rating: 4.5,
+            numReviews: 15,
+            description:'Made with dye'
+        },
+        {
+            _id:'3',
+            name:'Girl with stars',
+            category:'Shirts',
+            image:'/images/product-1.jpg',
+            price: 120,
+            rating: 4.5,
+            numReviews: 15,
+            description:'Made with dye'
+        },
+        {
+            _id:'3',
+            name:'Girl with stars',
+            category:'Shirts',
+            image:'/images/product-1.jpg',
+            price: 120,
+            rating: 4.5,
+            numReviews: 15,
+            description:'Made with dye'
+        }
+    ]
+}
+
+export default data;
